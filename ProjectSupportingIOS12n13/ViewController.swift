d63aff79c0ae96de398235bb3a148e69807923ce
Natalie <nwy0206@gmail.com>
2019-11-13 12:27:36 +0800

    Able to run normally on 12.2 but window is all black without any error msg on 13.1 simulator

---
+++ ProjectSupportingIOS12n13/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ProjectSupportingIOS12n13
+//
+//  Created by Natalie Ng on 2019/11/13.
+//  Copyright © 2019 Natalie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .lightGray
+    }
+    
+
+
+}
