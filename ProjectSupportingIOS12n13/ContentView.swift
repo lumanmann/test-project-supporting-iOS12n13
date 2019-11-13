d775116f07b83176ff3ec75ae50b367fa8968cdf
Natalie <nwy0206@gmail.com>
2019-11-13 12:00:29 +0800

    Init project

---
+++ ProjectSupportingIOS12n13/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectSupportingIOS12n13
+//
+//  Created by Natalie Ng on 2019/11/13.
+//  Copyright © 2019 Natalie. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
